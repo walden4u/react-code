fb6d8dca99c546609c59724035b984be6be75b62
Alex Kwon <walden4u@gmail.com>
2022-06-12 05:13:53 +0000

    ch1_5 props

--- ch1/index.js
+++ ch1/index.js
@@ -1,14 +1,17 @@
-const OurApp = () => {
+function OurApp() {
   return (
       <>
           <OurHeader />
-          <OurHeader />
           <TimeArea />
+          <ul>
+            <Pet name="Meowsalot" species="cat" age="5" />
+            <Pet name="Barksalot" species="dog" age="2" />
+            <Pet name="Fluffy" species="rabbit" age="3" />
+          </ul>
           <Footer />
       </>
-  );
+  )
 };
-
 
 const OurHeader = () => {
   return <h1 className="special">Our Amazing App Header Component</h1>;
@@ -17,6 +20,8 @@ const OurHeader = () => {
 const TimeArea = () => <p>The current time is {new Date().toLocaleString()}</p>;
 
 const Footer = () => <small>Copyright Footer Text!!!</small>;
+
+const Pet = (props) => <li>{props.name} is a {props.species} and is {props.age} years old.</li>
 
 
 setInterval(() => {ReactDOM.render(<OurApp />, document.querySelector('#app'));}, 1000);
